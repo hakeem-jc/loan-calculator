ce1ee6acb8b6b2b1474b77bc602011f5f36bc8f7
Hakeem Clarke <hakeem.clarke@speurgroup.com>
2025-01-19 19:00:32 -0500

    feat: Add form

---
+++ app/components/Input/index.tsx
@@ -0,0 +1,45 @@
+import { useForm } from "react-hook-form";
+import styles from "./styles.module.css";
+
+type InputProps = {
+  id: string;
+  label: string;
+  type: string;
+  register: ReturnType<typeof useForm>["register"];
+  error?: string;
+  placeholder?: string;
+  validationRules?: object;
+};
+
+const Input: React.FC<InputProps> = ({
+  id,
+  label,
+  type,
+  register,
+  error,
+  placeholder,
+  validationRules,
+}) => {
+  return (
+    <div className={`relative my-8 ${styles["float-label-input"]}`}>
+      <input
+        type={type}
+        id={id}
+        placeholder={placeholder ? placeholder : " "}
+        className={`block w-full bg-[#212121] focus:outline-none focus:shadow-outline border rounded-md py-3 px-3 leading-normal focus:border-blue-400 ${
+          error ? "border-red-500" : "border-gray-300"
+        }`}
+        {...register(id, validationRules)}
+      />
+      <label
+        htmlFor={id}
+        className="absolute top-3 left-1 text-gray-400 pointer-events-none transition duration-200 ease-in-out bg-[#212121] px-2"
+      >
+        {label}
+      </label>
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
+  );
+};
+
+export default Input;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,146 @@
+"use client";
+import { useState } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import Input from "./components/Input";
+
+type FormData = {
+  loanAmount: string;
+  interestRate: string;
+  lengthOfLoan: string;
+  paymentsPerYear: string;
+  calculatedLoanPayment: string;
+  totalNumberOfPayments: string;
+  amountPaid: string;
+  totalInterestPaid: string;
+};
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const calculate = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
-    <main className="flex items-center justify-center min-h-screen">
-      <h1 className="text-5xl">Temp</h1>
+    <main className="min-h-screen bg-[#181818] text-white">
+      <nav className="bg-white dark:bg-[#181818] w-full border-b border-gray-200 dark:border-gray-600">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a
+            href="#"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-8"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Lone
+            </span>
+          </a>
+        </div>
+      </nav>
+
+      <form className="max-w-xl flex flex-col mx-auto" onSubmit={calculate}>
+        <h2 className="text-xl pt-6">Basic Loan Information</h2>
+        <div className="  ">
+          <Input
+            id="loanAmount"
+            label="Loan Amount ($)"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.loanAmount?.message}
+            placeholder="1,000,000"
+            validationRules={{ required: "Loan Amount is required" }}
+          />
+
+          <Input
+            id="interestRate"
+            label="Interest Rate (%)"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.interestRate?.message}
+            placeholder="8.45"
+            validationRules={{ required: "Interest Rate is required" }}
+          />
+
+          <Input
+            id="lengthOfLoan"
+            label="Length of Loan in Years"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.calculatedLoanPayment?.message}
+            placeholder="60"
+            validationRules={{
+              required: "Length of Loan is required",
+            }}
+          />
+
+          <Input
+            id="paymentsPerYear"
+            label="Payments Per Year"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.paymentsPerYear?.message}
+            placeholder="12"
+            validationRules={{ required: "Payments Per Year is required" }}
+          />
+
+          <div className="text-center">
+            <button className="bg-blue-500 px-6 py-2 rounded-full hover:bg-blue-400">
+              Calculate
+            </button>
+          </div>
+        </div>
+
+        <div className="mt-8">
+          <h2 className="text-xl">Payment Information</h2>
+
+          <Input
+            id="calculatedLoanPayment"
+            label="Calculated Loan Payment"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.calculatedLoanPayment?.message}
+          />
+
+          <Input
+            id="totalNumberOfPayments"
+            label="Total Number of Payments"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.totalNumberOfPayments?.message}
+          />
+
+          <Input
+            id="amountPaid"
+            label="Amount Paid ($)"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.totalNumberOfPayments?.message}
+          />
+
+          <Input
+            id="totalInterestPaid"
+            label="Total Interest Paid ($)"
+            type="text"
+            // @ts-ignore
+            register={register}
+            error={errors.totalInterestPaid?.message}
+          />
+        </div>
+      </form>
     </main>
   );
 }
