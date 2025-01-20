951cec1b48a70d978179c0da9d5b56435667684b
Hakeem Clarke <hakeem.clarke@speurgroup.com>
2025-01-20 12:53:52 -0500

    style: Edit header

--- app/page.tsx
+++ app/page.tsx
@@ -53,13 +53,8 @@ export default function Home() {
             href="#"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              className="h-8"
-              alt="Flowbite Logo"
-            />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Lone
+              Lone Calc
             </span>
           </a>
         </div>
