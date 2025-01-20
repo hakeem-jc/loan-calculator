b63e5c9804f534654719e1b32ff35c3ba2266299
Hakeem Clarke <hakeem.clarke@speurgroup.com>
2025-01-20 12:54:51 -0500

    config: Update es-lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+   // Override or add custom rules
+  {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': ['off'], // Disable ts-ignore rule
+    },
+  },
 ];
 
 export default eslintConfig;
